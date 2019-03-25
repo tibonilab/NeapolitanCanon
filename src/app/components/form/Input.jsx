e55133f249c67ad9cf1d105ee135b464390a74b7
Alberto Tiboni <a.tiboni@gmail.com>
2019-03-25 16:48:41 +0100

    added first draft of form input component

---
+++ src/app/components/form/Input.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+export default class Input extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: props.value || ''
+        }
+    }
+
+    onChangeHandler(e) {
+        const value = e.target.value || '';
+
+        this.setState(
+            () => ({ value }),
+            () => this.emitValue(value)
+        )
+    }
+
+    emitValue(value) {
+        if (this.props.onChangeHandler && typeof this.props.onChangeHandler === 'function') {
+            this.props.onChangeHandler(value || this.state.value);
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <input 
+                    value={this.state.value}
+                    onChange={this.onChangeHandler.bind(this)}
+                    placeholder={this.props.placeholder}
+                />
+            </div>
+        )
+    }
+
+}
