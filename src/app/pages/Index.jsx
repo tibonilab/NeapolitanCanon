1cb44194671c0a0ceceea25d8853822d572c8bbc
Alberto Tiboni <a.tiboni@gmail.com>
2019-03-27 16:22:30 +0100

    clean up

--- src/app/pages/Index.jsx
+++ src/app/pages/Index.jsx
@@ -1,76 +1,17 @@
 import React, { Component } from 'react';
 
-import RestClient from '../service/RestClient';
+import { Link } from 'react-router-dom';
 
-import Input from './form/Input.jsx';
+import Template from '../components/template/Template.jsx';
 
 export default class Index extends Component {
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            list: [],
-            loading: false
-        }
-
-        this.debounce;
-    }
-
-    onInputChangeHandler(value) {
-
-        this.debounce && clearTimeout(this.debounce);
-
-        this.setState(
-            () => ({ 
-                list: [],
-                loading: true 
-            }),
-            () => this.debounce = setTimeout(() => RestClient
-                .get({ 
-                    url: '/solr/onstage/select',
-                    config: {params: {
-                        q: `interpreter_ss:${value}*`,
-                        rows: 100,
-                        wt: 'json'
-                    }}
-                })
-                .then(list => this.setState({ 
-                    list,
-                    loading: false
-                })                
-            ), 500)
-        );
-    }
-
-    renderLoading() {
-        return this.state.loading ? 'loading' : null
-    }
-
-    renderList() {
-        if(this.state.list.response && this.state.list.response.docs) {
-            return this.state.list.response.docs.map(element => (
-                <div key={element.id}>
-                    {element.title_s}, {element.place_s}
-                </div>
-            ))
-        }
-
-        return null;
-    };
-
     render() {
         return (
-            <div>
-                <h1>Hello World</h1>
-
-                <Input onChangeHandler={this.onInputChangeHandler.bind(this)} placeholder="Try ''Denger''..."/>                    
-
-                <div style={{padding: '1em 0'}}>
-                    {this.renderLoading()}
-                    {this.renderList()}
-                </div>
-            </div>
-        )
+            <Template>
+                <h1>Welcome</h1>
+                <Link to="/search">Seach</Link> or <Link to="/browse">Browse</Link>
+            </Template>
+        );
     }
 }
