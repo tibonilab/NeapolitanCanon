9ae761069f58e9bf3a8f3c5cd6c65ed8fea2032b
Alberto Tiboni <a.tiboni@gmail.com>
2019-03-27 16:17:24 +0100

    added sourc-map devtool

--- webpack.config.js
+++ webpack.config.js
@@ -2,54 +2,59 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const production = process.env.NODE_ENV === 'production';
+
 module.exports = {
-  entry: path.join(__dirname,'src','index.js'),
-  output: {
-    path: path.join(__dirname,'build'),
-    filename: 'index.bundle.js'
-  },
-  mode: process.env.NODE_ENV || 'development',
-  resolve: {
-    modules: [path.resolve(__dirname, 'src'), 'node_modules']
-  },
-  devServer: {
-    contentBase: path.join(__dirname,'src'),
-    proxy: {
-      '/solr' : {
-        target: 'http://192.168.1.107:8982/', // we temporarely fetch data from Rodolfo machine
-      },
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      }
-    }
-  },
-  module: {
-    rules: [
-      {
-        // this is so that we can compile any React,
-        // ES6 and above into normal ES5 syntax
-        test: /\.(js|jsx)$/,
-        // we do not want anything from node_modules to be compiled
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.(css|scss)$/,
-        use: [
-          "style-loader", // creates style nodes from JS strings
-          "css-loader", // translates CSS into CommonJS
-          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+    entry: path.join(__dirname,'src','index.js'),
+    output: {
+        path: path.join(__dirname,'build'),
+        filename: 'index.bundle.js',
+        publicPath: '/'
+    },
+    mode: process.env.NODE_ENV || 'development',
+    devtool: production ? null : 'source-map',
+    resolve: {
+        modules: [path.resolve(__dirname, 'src'), 'node_modules']
+    },
+    devServer: {
+        contentBase: path.join(__dirname,'src'),
+        historyApiFallback: true,
+        proxy: {
+            '/solr' : {
+                target: 'http://192.168.1.107:8983/', // we temporarely fetch data from Rodolfo machine
+            },
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        }
+    },
+    module: {
+        rules: [
+            {
+                // this is so that we can compile any React,
+                // ES6 and above into normal ES5 syntax
+                test: /\.(js|jsx)$/,
+                // we do not want anything from node_modules to be compiled
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    'style-loader', // creates style nodes from JS strings
+                    'css-loader', // translates CSS into CommonJS
+                    'sass-loader' // compiles Sass to CSS, using Node Sass by default
+                ]
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+                loaders: ['file-loader']
+            }
         ]
-      },
-      {
-        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-        loaders: ['file-loader']
-      }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname,'src','index.html')
+        })
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname,'src','index.html')
-    })
-  ]
 };
