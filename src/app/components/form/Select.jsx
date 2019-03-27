57486a941503855efea949bd791417bb0868756b
Alberto Tiboni <a.tiboni@gmail.com>
2019-03-27 16:19:16 +0100

    first draft of select form component

---
+++ src/app/components/form/Select.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Select extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: ''
+        };
+    }
+
+    onChangeHandler(e) {
+        const value = e.target.value;
+
+        this.setState({ value }, this.emit(value));
+    }
+
+    emit(value) {
+        if(this.props.onChangeHandler && typeof this.props.onChangeHandler === 'function') {
+            this.props.onChangeHandler(value);
+        }
+    }
+
+    render() {
+        const { options } = this.props;
+
+        return (
+            <select 
+                value={this.state.value}
+                onChange={this.onChangeHandler.bind(this)}
+            >
+                {
+                    this.props.placeholder ? (
+                        <option value="" disabled selected hidden>{this.props.placeholder}</option>
+                    ) : (
+                        <option value="" />
+                    )
+                }
+                {
+                    options.map(option => <option key={option.value} value={option.value}>{option.label}</option>)
+                }
+            </select>
+        );
+    }
+
+}
+
+Select.defaultProps = {
+    options: [],
+    placeholder: false
+};
+
+export default Select;
