3a7ed6677af841d48d462466cf661af9199fb171
Alberto Tiboni <a.tiboni@gmail.com>
2019-04-02 19:17:32 +0200

    first draft of facets navigation

--- src/app/pages/Search.jsx
+++ src/app/pages/Search.jsx
@@ -5,6 +5,9 @@ import Solr from '../model/Solr';
 import Template from '../components/template/Template.jsx';
 
 import Input from '../components/form/Input.jsx';
+import Select from '../components/form/Select.jsx';
+
+import { SEARCH_INDEXES, DEFAULT_FACETS } from '../model/INDEXES';
 
 export default class Search extends Component {
 
@@ -15,46 +18,75 @@ export default class Search extends Component {
             searchTerms: {
                 searchKey: '',
                 indexes: [],
-                facets: ['composer_ss', 'interpreter_ss', 'place_s', 'collection_s', 'series_s'],
+                facets: DEFAULT_FACETS,
+                filters: [],
                 dateRange: {}
             },
+            numFound: null,
             results: [],
             facets: [],
             loading: false
         };
 
-        let timeout;
-        this.debounce = (callback, milliseconds) => {
-            return (...args) => {
-                timeout && clearTimeout(timeout);
-                timeout = setTimeout(() => callback.apply(this, args), milliseconds);
-            };
-        };
+        // TODO: move this logic into autocompleter component
+        // let timeout;
+        // this.debounce = (callback, milliseconds) => {
+        //     return (...args) => {
+        //         timeout && clearTimeout(timeout);
+        //         timeout = setTimeout(() => callback.apply(this, args), milliseconds);
+        //     };
+        // };
+
+        this.onParamChangeHandler = this.onParamChangeHandler.bind(this);
     }
 
-    onInputChangeHandler(searchKey) {
+    onParamChangeHandler(param) {
+        return value => {
+            this.setState({ 
+                // TODO: refactor this switch with a helper or something else..
+                searchTerms: (
+                    () => {
+                        switch(param) {
+                        case 'searchKey': 
+                            return Object.assign({}, this.state.searchTerms, {
+                                [param]: value
+                            });
+                            
+                        case 'indexes': 
+                            return Object.assign({}, this.state.searchTerms, {
+                                [param]: value ? [value] : []
+                            });
+                        }
+                    }
+                )() 
+            });
+        };
 
-        const { facets, dateRange } = this.state.searchTerms;
+        // TODO: move this logic into autocompleter component
+        // () => this.debounce(this.performSearch, 500)()
+    }
 
+    setSolrResponse(solr) {
         this.setState({ 
+            numFound: solr.response.numFound,
+            results: solr.response.docs,
+            facets: solr.facet_counts ? solr.facet_counts.facet_fields : [],
+            loading: false 
+        });
+    }
+
+    performSearch() {
+        return Solr.search(this.state.searchTerms).then(this.setSolrResponse.bind(this));
+    }
+
+    onSearchButtonClickHandler(e) {
+        e && e.preventDefault();
+
+        this.setState({
+            numFound: 0,
             results: [], 
             loading: true 
-        },
-        () => this.debounce(
-            () => Solr
-                .search({ 
-                    searchKey, 
-                    dateRange,
-                    facets
-                })
-                .then(solr => this.setState({ 
-                    results: solr.response.docs,
-                    facets: solr.facet_counts ? solr.facet_counts.facet_fields : [],
-                    loading: false 
-                }))
-            , 500
-        )()
-        );
+        }, this.performSearch);
     }
 
     renderLoading() {
@@ -93,12 +125,39 @@ export default class Search extends Component {
         ));
     }
 
+    setFilter(field, value) {
+        this.setState({
+            searchTerms: Object.assign({}, this.state.searchTerms, {
+                filters: this.state.searchTerms.filters.concat(`${field}:${value}`)
+            })
+        }, this.performSearch);
+    }
+
+    unsetFilter(field, value) {
+        this.setState({
+            searchTerms: Object.assign({}, this.state.searchTerms, {
+                filters: this.state.searchTerms.filters.filter(f => f !== `${field}:${value}`)
+            })
+        }, this.performSearch);
+    }
+
+    toggleFilter(field, value) {
+        if (this.state.searchTerms.filters.includes(`${field}:${value}`)) {
+            this.unsetFilter(field, value);
+        } else {
+            this.setFilter(field, value);
+        }
+    }
+
     renderFacets() {
         return Object.keys(this.state.facets).map(key => (
             <div key={key}>
                 <br />
                 <h4>{key}</h4>
-                {Solr.normalizeFacetsResults(this.state.facets[key]).map((facet, index) => index < 10 && <div key={index}>{facet.label}, {facet.count}</div>)}
+                {Solr.normalizeFacetsResults(this.state.facets[key]).map((facet, index) => index < 10 && <div style={{display: 'flex', justifyContent:'space-between',cursor: 'pointer', background: this.state.searchTerms.filters.includes(`${key}:${facet.label}`) ? 'red' : 'transparent'}} key={index} onClick={e => this.toggleFilter(key, facet.label)}>
+                    <span>{facet.label}</span> 
+                    <span>{facet.count}</span>
+                </div>)}
             </div>
         ));
     }
@@ -108,28 +167,42 @@ export default class Search extends Component {
             <Template>
                 <h4>Search</h4>
 
-                <Input 
-                    onChangeHandler={this.onInputChangeHandler.bind(this)} 
-                />
+                <form onSubmit={this.onSearchButtonClickHandler.bind(this)} style={{display:'flex', jusityContent: 'flext-start'}}>
+                    <Input 
+                        onChangeHandler={this.onParamChangeHandler('searchKey')} 
+                    />
+                    <button type="submit">search</button>
+                </form>
+
+
+                <div>
+                    <h4>Advanced</h4>
+                    <Select 
+                        label="Search by index"
+                        placeholder="Select index"
+                        options={[{ label: 'Full-text', value: '' }].concat(SEARCH_INDEXES)}
+                        onChangeHandler={this.onParamChangeHandler('indexes')}
+                    />
+                </div>
 
                 <div style={{padding: '1em 0'}}>
                     {this.renderLoading()}
                     {
-                        this.state.results.length > 0 && (
+                        this.state.results.length > 0 ? (
                             <div style={{display:'flex', jusityContent: 'flext-start'}}>
-                                <div style={{padding: '2em'}}>
-                                    <h3>Search results</h3>
-                                    {this.renderSearchResults()}
-                                </div>
-                                <div style={{padding: '2em'}}>
+                                <div style={{padding: '2em 0', width: '300px'}}>
                                     <h3>Facets</h3>
                                     {this.renderFacets()}
                                 </div>
+                                <div style={{padding: '2em'}}>
+                                    <h3>Found {this.state.numFound} results.</h3>
+                                    {this.renderSearchResults()}
+                                </div>
                             </div>
-
+                        ) : (
+                            this.state.numFound != null && <h3>No results found</h3>
                         )
                     }
-
                 </div>
             </Template>
         );
