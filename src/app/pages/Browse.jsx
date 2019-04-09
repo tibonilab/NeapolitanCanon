88d2e683d7c87fdcdc9d8ad8ae156c25913f6e00
Alberto Tiboni <a.tiboni@gmail.com>
2019-04-09 13:42:17 +0200

    first draft of browse index navigation

--- src/app/pages/Browse.jsx
+++ src/app/pages/Browse.jsx
@@ -15,7 +15,8 @@ export default class Browse extends Component {
         this.state = {
             searchResults: [],
             browseResults: [],
-            loading: false
+            loading: false,
+            index: ''
         };
     }
 
@@ -24,7 +25,8 @@ export default class Browse extends Component {
             () => ({ 
                 searchResults: [], 
                 browseResults: [],
-                loading: true 
+                loading: true,
+                index
             }),
             () => Solr
                 .browse({ index, sort: 'index' })
@@ -36,10 +38,56 @@ export default class Browse extends Component {
         return this.state.loading ? 'loading' : null;
     }
 
+    search(searchKey) {
+        let payload = {};
+
+        if(this.state.index === 'year_i') {
+            payload = {
+                dateRange: {
+                    from: searchKey,
+                    to: searchKey
+                }
+            };
+        } else {
+            payload = {
+                searchKey,
+                indexes: [this.state.index]
+            };
+        }
+        
+        Solr
+            .search(payload)
+            .then(solr => this.setState(
+                () => ({ 
+                    searchResults: Object.assign({}, this.state.searchResults, {
+                        [searchKey]: solr.response.docs
+                    })
+                }), 
+                () => console.log(this.state.searchResults)
+            ));
+    }
+
     renderBrowseResults() {
         if(this.state.browseResults) {
-            return this.state.browseResults.map((term, index) => (
-                index % 2 === 0 && <div key={index}>{term}</div>
+            return Solr.normalizeFacetsResults(this.state.browseResults).map((term, index) => (
+                <div key={index} onClick={() => this.search(term.label)} style={{cursor:'pointer'}}>
+                    <h4>{term.label}</h4>
+                    {
+                        this.state.searchResults[term.label] && (
+                            <div>
+                                {
+                                    this.state.searchResults[term.label].map(element => (
+                                        <div key={element.id}>
+                                            <br />
+                                            <h2>{element.title_s}</h2> 
+                                            <h3>{element.place_s} - {element.year_i}</h3>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        )
+                    }
+                </div>
             ));
         }
 
@@ -52,6 +100,7 @@ export default class Browse extends Component {
 
                 <h4>Browse</h4>
                 <Select
+                    value={this.state.index}
                     placeholder="Select index"
                     options={BROWSE_INDEXES}
                     onChangeHandler={this.onSelectChangeHandler.bind(this)} 
