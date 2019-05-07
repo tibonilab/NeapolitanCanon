d33606484874b07f7a35354909654610f0f2e829
Alberto Tiboni <a.tiboni@gmail.com>
2019-05-07 12:22:17 +0200

    using GlobalState in the main App component

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Router from './app/Router.jsx';
+import GlobalState from './app/context/GlobalState.jsx';
 
-const App = () => <Router />;
+const App = () => (
+    <GlobalState>
+        <Router />
+    </GlobalState>
+);
 
 ReactDOM.render(
     <App />,
