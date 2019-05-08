c8b134faa5ea39be9ed0603beb40cc40cfe82fb9
Alberto Tiboni <a.tiboni@gmail.com>
2019-05-08 19:16:40 +0200

    adding search and browse contexts

---
+++ src/app/context/browseContext.js
@@ -0,0 +1,9 @@
+import { createContext } from 'react';
+
+export default createContext({
+    searchResults: {},
+    browseResults: [],
+    browseTerms: {},
+    isLoading: false,
+    selectedIndex: '',
+});
