0bff0aceea06a4e1e42297568bbb526dea50c631
Alberto Tiboni <a.tiboni@gmail.com>
2019-05-08 19:19:54 +0200

    first draft of navbar

--- src/app/components/template/Template.jsx
+++ src/app/components/template/Template.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import '../../../index.scss';
 
@@ -7,6 +8,9 @@ export default class Template extends Component {
     render() {
         return (
             <div className="template-root">
+                <div style={{ float: 'right' }}>
+                    <Link to="/search">Search</Link> or <Link to="/browse">Browse</Link>
+                </div>
                 {this.props.children}
             </div>
         );
