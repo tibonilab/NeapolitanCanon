b754a7d3881967926a438aaa04b433d234239c43
Alberto Tiboni <a.tiboni@gmail.com>
2019-06-11 09:31:21 +0200

    first draft of Navbar component

---
+++ src/app/components/template/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Navbar = () => (
+    <div className="navbar-root">
+        <img src="http://d-lib.rism-ch.org/onstage/images/logo_trans-75-b.png" style={{ maxHeight: '38px' }} />
+    </div>
+);
