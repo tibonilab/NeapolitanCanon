b311fb6db48c947b7c03abae019ea6c4d563b1d2
Alberto Tiboni <a.tiboni@gmail.com>
2019-06-19 15:08:01 +0200

    using Buttons scss

--- src/app/components/template/components/Buttons.jsx
+++ src/app/components/template/components/Buttons.jsx
@@ -1,3 +1,7 @@
 import React from 'react';
 
+import './Buttons.scss';
+
 export const ClearButton = props => <button className={`clearButton-root${props.isActive ? ' clearButton__active' : ''}`}>{props.children}</button>;
+
+export const PrimaryButton = props => <button className={'primaryButton-root'} disabled={props.disabled} type={props.type}>{props.children}</button>;
