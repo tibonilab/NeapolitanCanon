dceedde3c27114568cec7eddcb15064e1ec68834
Alberto Tiboni <a.tiboni@gmail.com>
2019-08-06 22:45:24 +0200

    better ListBox animation

--- src/app/components/template/components/ListBox.jsx
+++ src/app/components/template/components/ListBox.jsx
@@ -7,15 +7,35 @@ const ListBox = props => {
 
     const [collapsed, setCollapsed] = useState(false);
 
-    const classNames = ['listBox-root'];
+    const [classNames, setClassNames] = useState(['listBox-root']);
 
-    if (collapsed) {
-        classNames.push('listBox__collapsed');
-    }
+    let animationTimer;
+
+    const toggleCollapsed = () => {
+        console.log('toggleCollapsed');
+        if (!collapsed) {
+            setCollapsed(true);
+            setClassNames([
+                ...classNames,
+                'listBox__collapsed'
+            ]);
+            animationTimer = setTimeout(() => {
+                setClassNames([
+                    ...classNames,
+                    'listBox__collapsed',
+                    'listBox__collapsed-finished'
+                ]);
+            }, 200);
+        } else {
+            setCollapsed(false);
+            setClassNames(['listBox-root']);
+            clearTimeout(animationTimer);
+        }
+    };
 
     return (
         <div className={classNames.join(' ')}>
-            <div className="listBox-header" onClick={() => setCollapsed(!collapsed)}>
+            <div className="listBox-header" onClick={toggleCollapsed}>
                 {props.header}
             </div>
 
