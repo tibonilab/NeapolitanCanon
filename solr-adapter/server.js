253ada65fc944e075299294e3ee5fd426979567f
Rodolfo Zitellini <rwz@xhero.org>
2019-08-13 14:54:07 +0200

    Enable CORS

--- solr-adapter/server.js
+++ solr-adapter/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors')
 const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Enable CORS for everybody
+app.use(cors())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
